8e1a909b7fcf3a8d354c5c666e9b21b5fed4fba5
derend <deren.dohoda@gmail.com>
2015-07-22 10:41:26 -0400

    creation
    creation

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define version "1.0")
+(define collection "mini-db")
+
+(define pkg-desc "Syntax providing functions for simple automatic data management.")
+
+(define deps '())
+(define build-deps '())
